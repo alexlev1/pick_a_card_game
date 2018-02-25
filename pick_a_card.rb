494d90bbf9f009150f9dfc3055dc20c3e1a576f8
Alex Levashov <alevash1@gmail.com>
2018-02-25 16:28:09 +0300

    Add suits in Unicode

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ["\u2660", "\u2661", "\u2663", "\u2662"]
 
 puts "And your card is..."
-puts "#{value.sample} of #{suits.sample}"
+puts "#{value.sample}#{suits.sample.encode('utf-8')}"
